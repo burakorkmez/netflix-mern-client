d8a94302de16ef1bb3499df7569524b5aeca3453
Burak Örkmez <burakorkmezz@gmail.com>
2022-01-14 19:57:59 +0100

    added login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 
 function App() {
 	return (
 		<div className="App">
-			<Register />
+			<Login />
 		</div>
 	);
 }
