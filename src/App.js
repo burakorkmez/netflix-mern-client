6251de696af72e07b45f2044377470141db18866
Burak Örkmez <burakorkmezz@gmail.com>
2022-01-14 19:50:47 +0100

    added register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.scss';
 import Home from './pages/home/Home';
-import Watch from './pages/watch/Watch';
+import Register from './pages/register/Register';
 
 function App() {
 	return (
 		<div className="App">
-			<Watch />
+			<Register />
 		</div>
 	);
 }
