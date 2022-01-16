2b2a09520301a3ecae2207b24fb64cfdef2028fb
Burak Örkmez <burakorkmezz@gmail.com>
2022-01-16 16:41:53 +0100

    added routes and links

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import './App.scss';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import Watch from './pages/watch/Watch';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
+	const user = true;
 	return (
-		<div className="App">
-			<Login />
-		</div>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/login">
+					{!user ? <Login /> : <Redirect to="/" />}
+				</Route>
+				<Route exact path="/register">
+					{!user ? <Register /> : <Redirect to="/" />}
+				</Route>
+				<Route exact path="/">
+					{user ? <Home /> : <Redirect to="/login" />}
+				</Route>
+
+				<Route exact path="/movies">
+					{user ? <Home type={'movies'} /> : <Redirect to="/login" />}
+				</Route>
+				<Route exact path="/series">
+					{user ? <Home type={'series'} /> : <Redirect to="/login" />}
+				</Route>
+				<Route exact path="/watch">
+					{user ? <Watch /> : <Redirect to="/login" />}
+				</Route>
+			</Switch>
+		</BrowserRouter>
 	);
 }
 
--- src/pages/watch/Watch.js
+++ src/pages/watch/Watch.js
@@ -1,13 +1,16 @@
 import { ArrowBackOutlined } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 import './watch.scss';
 
 export default function Watch() {
 	return (
 		<div className="watch">
-			<div className="back">
-				<ArrowBackOutlined />
-				Home
-			</div>
+			<Link to="/">
+				<div className="back">
+					<ArrowBackOutlined />
+					Home
+				</div>
+			</Link>
 			<video
 				className="video"
 				autoPlay
